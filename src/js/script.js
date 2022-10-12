6fdeec7d971b2735b769eaece2d0a135c46d85c1
MuhammadZakyR <muhammadzaky.19058@mhs.unesa.ac.id>
2022-10-12 12:04:16 +0700

    Add Dummy Content

---
+++ src/js/script.js
@@ -0,0 +1,4 @@
+// Get API
+const movieUrl = "https://api.themoviedb.org/3/discover/movie?&api_key=88ef544735216b8d23c937d6adbce2eb";
+const searchMovieUrl = "https://api.themoviedb.org/3/search/movie?&api_key=88ef544735216b8d23c937d6adbce2eb";
+
